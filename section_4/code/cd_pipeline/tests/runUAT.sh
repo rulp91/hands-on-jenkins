4a1b953df625bd2a10b87b2a682e9b471056a411
rperez_coaat <rperez@coaat.es>
2023-01-31 19:50:47 +0100

    Prueba de impresión

--- section_4/code/cd_pipeline/tests/runUAT.sh
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -5,7 +5,10 @@ hostname='localhost'
 port=$1
 
 # wait for the app to start
-sleep 15 
+sleep 5 
+
+status1=$(curl --write-out "%{http_code}" --silent --output /dev/null "${hostname}:${port}")
+echo "$status1" # Would give you just the status
 
 # ping the app
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
