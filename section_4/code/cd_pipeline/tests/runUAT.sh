989b48a7f7a6f3b1e2d3fe7c2ecff4cb015247e6
rperez_coaat <rperez@coaat.es>
2023-01-31 19:59:41 +0100

    Prueba de impresión

--- section_4/code/cd_pipeline/tests/runUAT.sh
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -7,12 +7,14 @@ port=$1
 # wait for the app to start
 sleep 5 
 
-status1=$(curl --write-out "%{http_code}" --out /dev/null --silent "${hostname}:${port}")
+status1=$(curl --write-out "%{http_code}" --silent --output /dev/null "${hostname}:${port}")
 echo "$status1" # Would give you just the status
+
+STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8888)
+echo "$STATUS"
 
 # ping the app
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
-echo "$status_code" # Would give you just the status
 
 if [ $status_code == 200 ];
 then
